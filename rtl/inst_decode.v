dc9263b61da13342bfd6163a98526f38e8a06732
gtxzsxxk <2524395907@qq.com>
2023-11-28 15:37:26 +0800

    fix: the right position of bubble

--- rtl/inst_decode.v
+++ rtl/inst_decode.v
@@ -25,7 +25,6 @@ parameter ALGORITHM = 7'b0110011;
 parameter ALGORITHM_IMM = 7'b0010011;
 parameter LOAD = 7'b0000011;
 
-reg [31:0] inst_last = 32'h00000013;
 reg [63:0] registers[31:0];
 integer rst_i;
 
@@ -92,15 +91,29 @@ always @ (posedge CLK or negedge reset) begin
         for(rst_i = 0;rst_i<32;rst_i=rst_i+1) begin
             registers[rst_i] <= 64'd0;
         end
-        inst_last <= 32'h00000013;
     end
     else begin
-        instruction <= get_inst(inst,stall);
         if(wb_en) begin
             registers[wb_rd] <= wb_value;
         end
         registers[0] <= 64'd0;
-        inst_last <= instruction;
+
+        if(inst[6:0] == ALGORITHM) begin
+            stall_raise <= judge_stall(instruction[6:0],
+                inst[19:15], inst[24:20], 0);
+            instruction <= get_inst(inst,stall | judge_stall(instruction[6:0],
+                inst[19:15], inst[24:20], 0));
+        end
+        else if(inst[6:0] == ALGORITHM_IMM) begin
+            stall_raise <= judge_stall(instruction[6:0],
+                inst[19:15], 0, 1);
+            instruction <= get_inst(inst,stall | judge_stall(instruction[6:0],
+                inst[19:15], 0, 1));
+        end
+        else if(inst[6:0] == LOAD) begin
+            stall_raise <= 0;
+            instruction <= get_inst(inst,stall);
+        end
     end
 end
 
@@ -117,8 +130,6 @@ always @ (negedge CLK) begin
         load_flag <= 0;
         write_back <= 1;
         imm_flag <= 0;
-        stall_raise <= judge_stall(inst_last[6:0],
-            instruction[19:15], instruction[24:20], 0);
     end
     else if(instruction[6:0] == ALGORITHM_IMM) begin
         rd <= instruction[11:7];
@@ -131,8 +142,6 @@ always @ (negedge CLK) begin
         load_flag <= 0;
         write_back <= 1;
         imm_flag <= 1;
-        stall_raise <= judge_stall(inst_last[6:0],
-            instruction[19:15], 0, 1);
     end
     else if(instruction[6:0] == LOAD) begin
         rd <= instruction[11:7];
@@ -145,7 +154,6 @@ always @ (negedge CLK) begin
         load_flag <= 1;
         write_back <= 1;
         imm_flag <= 1;
-        stall_raise <= 0;
     end
 end
 
